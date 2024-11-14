352af86c1eebd35c1203bffc14a44e5041d591bf
Raihan Kabir <r.kabir01@protonmail.com>
2024-11-14 19:22:33 +0600

    python 3.6 to <4

--- setup.py
+++ setup.py
@@ -3,4 +3,5 @@ from setuptools import setup, find_packages
 setup(
     packages=find_packages(where=["uscodekit"]),
     options={"bdist_wheel": {"universal": False}},
+    python_requires=">=3.6, <4",
 )
